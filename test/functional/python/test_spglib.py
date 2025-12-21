fe0e1397a277ebab20d58afa369165c4cf9006a6
Atsushi Togo <atz.togo@gmail.com>
2025-12-21 16:26:28 +0900

    Move error handling tests

--- test/functional/python/test_spglib.py
+++ test/functional/python/test_spglib.py
@@ -316,31 +316,3 @@ def test_magnetic_symmetry_database():
     }
     for key in ["rotations", "translations", "time_reversals"]:
         assert np.allclose(data_r_actual[key], data_r_expect[key])
-
-
-@pytest.mark.parametrize("value", ["0", "1", "true", "false", "True", "False"])
-def test_deprecation_warning_env(monkeypatch, value: str):
-    monkeypatch.setenv("SPGLIB_OLD_ERROR_HANDLING", value)
-
-    if value in ("1", "true", "True"):
-        with pytest.warns(DeprecationWarning):
-            warnings.simplefilter("always", DeprecationWarning)
-            niggli_reduce([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-    else:
-        with warnings.catch_warnings():
-            warnings.simplefilter("error")
-            niggli_reduce([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-
-
-@pytest.mark.parametrize("value", [True, False])
-def test_deprecation_warning_OLD_ERROR_HANDLING(monkeypatch, value: bool):
-    monkeypatch.setattr(spglib.error, "OLD_ERROR_HANDLING", value)
-
-    if value:
-        with pytest.warns(DeprecationWarning):
-            warnings.simplefilter("always", DeprecationWarning)
-            niggli_reduce([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-    else:
-        with warnings.catch_warnings():
-            warnings.simplefilter("error")
-            niggli_reduce([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
